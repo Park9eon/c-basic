acd74cea0bc4ea8cadb63eb13788b4aa223e8c12
Park9eon <quiet9.noise@gmail.com>
2019-01-29 11:18:45 +0900

    Single Pointer 연습

--- src/Pointer.c
+++ src/Pointer.c
@@ -4,6 +4,25 @@
 
 #include <stdio.h>
 
+void updatePoint(int *p) {
+    *p = 10;
+}
+
+void updateValue(int v) {
+    v = 10;
+}
+
 int main() {
+    int a = 1;
+    int *b = &a;
+
+    printf("a is %d\n", a);
+    printf("b is %p\n", b);
+    printf("*b is %d\n", *b);
+
+    updateValue(a);
+    printf("a is %d\n", a);
+    updatePoint(&a);
+    printf("a is %d\n", a);
     return 1;
 }
